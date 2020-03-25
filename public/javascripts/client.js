03002537aa07fc518badb3448aba80bc2ffdef05
julianatse <juliana.tse@seasonalife.com>
2020-03-26 05:22:11 +0800

    add predefined patterns

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,37 +1,57 @@
 const ws = new WebSocket("ws://localhost:3030");
 var clientColor;
+
 ws.onmessage = function incoming(message) {
   var json = JSON.parse(message.data);
   if (json.type == "color") {
-    // console.log(json);
     window.clientColor = json.value;
+    alert(
+      "Please select the cells within 5 secs (the board will be frozen for cells selection)"
+    );
   } else if (json.type == "cellColors") updateGrid(json.value);
-  else console.log("Unrecognized message from server");
-  // console.log("message");
-  console.log(json.value);
+  else if (json.type == "patternCells") {
+    updateGrid(json.value);
+    ws.send(JSON.stringify({ type: "next", color: window.clientColor }));
+  } else console.log("Unrecognized message from server");
 };
 
-function updateGrid(cellColors) {
+const blinker = () =>
+  ws.send(
+    JSON.stringify({
+      type: "pattern",
+      pattern: "blinker",
+      color: window.clientColor
+    })
+  );
+const block = () =>
+  ws.send(
+    JSON.stringify({
+      type: "pattern",
+      pattern: "block",
+      color: window.clientColor
+    })
+  );
+const glider = () =>
+  ws.send(
+    JSON.stringify({
+      type: "pattern",
+      pattern: "glider",
+      color: window.clientColor
+    })
+  );
+
+const updateGrid = cellColors => {
   var x = document.body.getElementsByClassName("grid");
   for (r = 0; r < 20; ++r) {
     for (c = 0; c < 20; ++c) {
-      x[0].rows[r].cells[c].style.backgroundColor = cellColors[r * 20 + c - 1];
+      x[0].rows[r].cells[c].style.backgroundColor = cellColors[r * 20 + c];
     }
   }
-}
+};
 
-var grid = clickableGrid(20, 20, function(el, row, col, i) {
-  // console.log("You clicked on element:", el);
-  // console.log("You clicked on row:", row);
-  // console.log("You clicked on col:", col);
-  // console.log("You clicked on item #:", i);
-  el.style.backgroundColor = window.clientColor;
-  ws.send(i);
-});
+var onClickTimeout;
 
-document.body.appendChild(grid);
-
-function clickableGrid(rows, cols, callback) {
+const clickableGrid = (rows, cols, callback) => {
   var i = 0;
   var grid = document.createElement("table");
   grid.className = "grid";
@@ -42,14 +62,28 @@ function clickableGrid(rows, cols, callback) {
       var i = r * 20 + c;
       cell.addEventListener(
         "click",
-        (function(el, r, c, i) {
+        (function(el, i) {
           return function() {
-            callback(el, r, c, i);
+            callback(el, i);
           };
-        })(cell, r, c, i),
+        })(cell, i),
         false
       );
     }
   }
   return grid;
-}
+};
+
+var grid = clickableGrid(20, 20, function(el, i) {
+  clearTimeout(onClickTimeout);
+  el.style.backgroundColor = window.clientColor;
+  ws.send(
+    JSON.stringify({ type: "click", cellIndex: i, color: window.clientColor })
+  );
+  setTimeout(
+    () => ws.send(JSON.stringify({ type: "next", color: window.clientColor })),
+    5000
+  );
+});
+
+document.getElementById("grid-div").appendChild(grid);
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const WebSocket = require("ws");
+const _ = require("lodash");
 
 const PORT = process.env.PORT || 3030;
 const INDEX = "/public/index.html";
 const app = express();
+const white = "rgb(255,255,255)";
 
 const server = app
   .use(express.static("public"))
@@ -12,107 +14,183 @@ const server = app
 
 const wss = new WebSocket.Server({ server });
 
-wss.getUniqueID = function() {
-  function s4() {
+wss.getUniqueID = () => {
+  const s4 = () => {
     return Math.floor((1 + Math.random()) * 0x10000)
       .toString(16)
       .substring(1);
-  }
+  };
   return s4() + s4() + "-" + s4();
 };
 
-var gameTimeout;
-var messageRecv = false;
-// use array of cell colors
 // initialize all cell colors as white
-const cellColors = Array.apply(null, Array(400)).map(_ => "#FFFFFF");
+const cellColors = Array.apply(null, Array(400)).map(_ => white);
 wss.on("connection", ws => {
   console.log("Client connected");
   // when new user has chosen the cells
   ws.id = wss.getUniqueID();
   ws.color = generateRandomColor();
-  wss.clients.forEach(function each(client) {
+  wss.clients.forEach(client => {
     console.log("Client.ID: " + client.id);
     console.log("Client.Color: " + client.color);
   });
   ws.send(JSON.stringify({ type: "color", value: ws.color }));
   ws.send(JSON.stringify({ type: "cellColors", value: cellColors }));
-  ws.on("message", function(message) {
-    clearTimeout(gameTimeout);
-    const cellNumber = message;
-    cellColors[cellNumber - 1] = ws.color;
-    ws.send(JSON.stringify({ type: "cellColors", value: cellColors }));
-    messageRecv = true;
-    console.log("messageRecv: ", messageRecv);
-    gameTimeout = setTimeout(() => {
-      nextGeneration(ws.color);
-      console.log("server send cellColors waiting for next onclick");
+  ws.on("message", message => {
+    var jsonMsg = JSON.parse(message);
+    if (jsonMsg.type === "next") {
+      setInterval(() => {
+        nextGeneration();
+        produceNextGrid();
+        ws.send(JSON.stringify({ type: "cellColors", value: cellColors }));
+      }, 1000);
+    } else if (jsonMsg.type === "pattern") {
+      var emptyCellIndex = cellColors.findIndex(color => color === white);
+      switch (jsonMsg.pattern) {
+        case "blinker":
+          blinker(jsonMsg.color, emptyCellIndex);
+          break;
+        case "block":
+          block(jsonMsg.color, emptyCellIndex);
+          break;
+        case "glider":
+          glider(jsonMsg.color, emptyCellIndex);
+          break;
+      }
+      ws.send(JSON.stringify({ type: "patternCells", value: cellColors }));
+    } else {
+      console.log(jsonMsg.cellIndex, jsonMsg.color);
+      cellColors[jsonMsg.cellIndex] = jsonMsg.color;
       ws.send(JSON.stringify({ type: "cellColors", value: cellColors }));
-    }, 1000);
+    }
   });
-  console.log("messageRecvAfter: ", messageRecv);
-  //   console.log("no more onclicks");
-  if (messageRecv) {
-    console.log("messageRecv is true");
-    setInterval(() => {
-      nextGeneration(ws.color);
-      console.log("server send cellColors after no onclick");
-      ws.send(JSON.stringify({ type: "cellColors", value: cellColors }));
-    }, 1000);
-  }
   // remove client info when it is disconnected
   ws.on("close", () => console.log("Client disconnected"));
 });
 
+// blinker
+const blinker = (color, emptyCellIndex) => {
+  if (emptyCellIndex % 20 === 0) emptyCellIndex++;
+  if (emptyCellIndex % 20 === 19) emptyCellIndex--;
+  cellColors[emptyCellIndex] = color;
+  const secondCellIndex =
+    emptyCellIndex - 20 >= 0 ? emptyCellIndex - 20 : emptyCellIndex + 40;
+  cellColors[secondCellIndex] = color;
+  const thirdCellIndex =
+    emptyCellIndex + 20 <= 399 ? emptyCellIndex + 20 : emptyCellIndex - 40;
+  cellColors[thirdCellIndex] = color;
+};
+// block
+const block = (color, emptyCellIndex) => {
+  const negateC = emptyCellIndex % 20 < 19 ? 1 : -1;
+  const negateR = emptyCellIndex - 20 < 0 ? 1 : -1;
+  for (r = 0; r < 2; r++) {
+    for (c = 0; c < 2; c++) {
+      cellColors[emptyCellIndex + negateR * r * 20 + c * negateC] = color;
+    }
+  }
+};
+// glider
+const glider = (color, emptyCellIndex) => {};
+
 // generate random color
 function generateRandomColor() {
-  var color =
-    "#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
+  var randomRGB = [];
+  for (i = 0; i < 3; i++) {
+    randomRGB.push(Math.floor(Math.random() * 256));
+  }
+  const color = `rgb(${randomRGB.join(",")})`;
   return color;
 }
 
-function isAlive(cellNumber) {
-  return cellColors[cellNumber - 1] !== "#FFFFFF";
-}
+const isAlive = cellNumber => cellColors[cellNumber] !== white;
 
-function neighbours(row, col) {
-  // out of boundary condition
+const colorAverage = colors => {
+  return (
+    "rgb(" +
+    _.zip
+      .apply(
+        null,
+        colors.map(rgb => rgb.match(/\d+/g).map(d => parseInt(d)))
+      )
+      .map(d => ~~(d.reduce((acc, curr) => acc + curr, 0) / 3))
+      .join(",") +
+    ")"
+  );
+};
+
+const neighbours = (row, col) => {
   var count = 0;
+  var neighbourColorList = [];
   // left upper
-  if (isAlive(cellColors[(row - 1) * 20 + col - 1])) count += 1;
+  if (isAlive((row - 1) * 20 + col - 1) && row > 0 && col > 0) {
+    count++;
+    neighbourColorList.push(cellColors[(row - 1) * 20 + col - 1]);
+  }
   // upper
-  if (isAlive(cellColors[(row - 1) * 20 + col])) count += 1;
+  if (isAlive((row - 1) * 20 + col) && row > 0) {
+    count++;
+    neighbourColorList.push(cellColors[(row - 1) * 20 + col]);
+  }
   // right upper
-  if (isAlive(cellColors[(row - 1) * 20 + col + 1])) count += 1;
+  if (isAlive((row - 1) * 20 + col + 1) && row > 0 && col < 19) {
+    count++;
+    neighbourColorList.push(cellColors[(row - 1) * 20 + col + 1]);
+  }
   // left
-  if (isAlive(cellColors[row * 20 + (col - 1)])) count += 1;
+  if (isAlive(row * 20 + (col - 1)) && col > 0) {
+    count++;
+    neighbourColorList.push(cellColors[row * 20 + (col - 1)]);
+  }
   // right
-  if (isAlive(cellColors[row * 20 + col + 1])) count += 1;
+  if (isAlive(row * 20 + col + 1) && col < 19) {
+    count++;
+    neighbourColorList.push(cellColors[row * 20 + col + 1]);
+  }
   // left lower
-  if (isAlive(cellColors[(row + 1) * 20 + col - 1])) count += 1;
+  if (isAlive((row + 1) * 20 + col - 1) && row < 19 && col > 0) {
+    count++;
+    neighbourColorList.push(cellColors[(row + 1) * 20 + col - 1]);
+  }
   // lower
-  if (isAlive(cellColors[(row + 1) * 20 + col])) count += 1;
+  if (isAlive((row + 1) * 20 + col) && row < 19) {
+    count++;
+    neighbourColorList.push(cellColors[(row + 1) * 20 + col]);
+  }
   // right lower
-  if (isAlive(cellColors[(row + 1) * 20 + col + 1])) count += 1;
-  //   console.log(count);
-  return count;
-}
+  if (isAlive((row + 1) * 20 + col + 1) && row < 19 && col < 19) {
+    count++;
+    neighbourColorList.push(cellColors[(row + 1) * 20 + col + 1]);
+  }
+  // find average of colors
+  const averageColor = count === 3 ? colorAverage(neighbourColorList) : "";
+  return { count, averageColor };
+};
 
+var cellsToBeModified = [];
 // generate next generation
-function nextGeneration(color) {
+const nextGeneration = () => {
   for (r = 0; r < 20; ++r) {
     for (c = 0; c < 20; ++c) {
       const cellIndex = r * 20 + c;
-      if (!isAlive(cellIndex) && neighbours(r, c) == 3) {
-        console.log("case 1");
-        cellColors[cellIndex - 1] = color;
-      } else if (
-        isAlive(cellIndex) &&
-        (neighbours(r, c) < 2 || neighbours(r, c) > 3)
-      ) {
-        console.log("case 2");
-        cellColors[cellIndex - 1] = "#FFFFFF";
-      }
+      const { count, averageColor } = neighbours(r, c);
+      if (!isAlive(cellIndex) && count === 3) {
+        cellsToBeModified.push({
+          cellIndex: cellIndex,
+          color: averageColor
+        });
+      } else if (isAlive(cellIndex) && (count < 2 || count > 3))
+        cellsToBeModified.push({
+          cellIndex: cellIndex,
+          color: white
+        });
     }
   }
-}
+};
+
+const produceNextGrid = () => {
+  cellsToBeModified.forEach(cellObject =>
+    cellColors.splice(cellObject.cellIndex, 1, cellObject.color)
+  );
+  cellsToBeModified = [];
+};
